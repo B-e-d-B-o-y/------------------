96af39754a35889ce0cbb428e70bfe692f77d517
B-e-d-B-o-y <IgorPerminov2004@mail.ru>
2024-07-13 12:29:05 +0900

    Создание страницы карточка

--- src/components/card.vue
+++ src/components/card.vue
@@ -56,6 +56,141 @@
             <input class="inputcard" id="email2">
         </div>
     </form></div>
+
+    <div class="bordercard">
+    <label class="labelcard">Тип знака</label>
+    <div class="inputRadioBig">
+                <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Сигнал</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Пирамида</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-3" type="radio" name="radio" value="3">
+                    <label for="radio-3">Штатив</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-4" type="radio" name="radio" value="4">
+                    <label for="radio-4">Тур</label>
+                </div></div>
+
+        <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+            <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>        
+
+
+        <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+                <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>     
+
+
+        <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+            <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>        
+
+
+        <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+                <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>    
+
+
+                <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+            <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>    
+
+
+        <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+                <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>    
+
+                <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+            <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div>        
+
+
+        <div class="labelcardwrapper">  
+        <label class="labelcard">Опознавательный столб (знак)</label>
+        <div class="inputRadio">
+                <div class="form_radio_btn">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">Устанавливается</label>
+                </div>
+                        
+                <div class="form_radio_btn">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">Не устанавливается</label>
+                </div></div></div></div>
+
+
     <div class="bordercard"><div class="inputPairBig">
         <label for="email2">Примечания</label>
         <input class="inputcardBig" id="email2">
@@ -118,4 +253,64 @@
     margin: 2%;
 }
 
+.form_radio_btn {
+	display: inline-block;
+    padding: 1% 6%;
+}
+
+.form_radio_btn input[type=radio] {
+	display: none;
+}
+.form_radio_btn label {
+	display: inline-block;
+	cursor: pointer;
+	border: none;
+    font-size: $cardFontSize;
+	user-select: none;
+    color: $mainFontColor;
+}
+ 
+/* Checked */
+.form_radio_btn input[type=radio]:checked + label {
+	color: $greenColor;
+    border-bottom: 2px solid $greenColor;
+}
+ 
+/* Hover */
+.form_radio_btn label:hover {
+	color: $grayColor;
+    border-bottom: 2px solid $grayColor;
+}
+ 
+/* Disabled */
+.form_radio_btn input[type=radio]:disabled + label {
+	background: #efefef;
+	color: #666;
+}
+
+.inputRadioBig{
+    border-radius: 100px;
+    border: 3px solid $grayColor;
+    padding: 1.4%;
+    width: 94%;
+    margin: 0% 3% 5% 3%;
+}
+
+.labelcard{
+    color: $mainFontColor;
+    display: flex;
+    margin: 0% 3%;
+}
+
+.inputRadio{
+    border-radius: 100px;
+    border: 3px solid $grayColor;
+    padding: 2% 0%;
+}
+
+.labelcardwrapper{
+    display: inline-block;
+    width: 44%;
+    margin: 2% 3%;
+}
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,33 @@
 <template>
     <header>
-        <button class="buttonheader">Главная</button>
-        <button class="buttonheader">Карточка</button>
-        <button class="buttonheader">Помощь</button>
-        <button class="buttonheader">Карта</button>
-        <button class="buttonheader">Проверка</button>
-        <button class="buttonheader">Профиль</button>
+                <div class="form_radio_btn_header">
+                    <input id="radioheader_main" type="radio" name="radioheader" value="1" checked>
+                    <label for="radioheader_main">Главная</label>
+                </div>
+                        
+                <div class="form_radio_btn_header">
+                    <input id="radioheader_card" type="radio" name="radioheader" value="2">
+                    <label for="radioheader_card">Карточка</label>
+                </div>
+                        
+                <div class="form_radio_btn_header">
+                    <input id="radioheader_remember" type="radio" name="radioheader" value="3">
+                    <label for="radioheader_remember">Помощь</label>
+                </div>
+                        
+                <div class="form_radio_btn_header">
+                    <input id="radioheader_map" type="radio" name="radioheader" value="4">
+                    <label for="radioheader_map">Карта</label>
+                </div>
+                <div class="form_radio_btn_header">
+                    <input id="radioheader_check" type="radio" name="radioheader" value="5">
+                    <label for="radioheader_check">Проверка</label>
+                </div>
+                        
+                <div class="form_radio_btn_header">
+                    <input id="radioheader_profile" type="radio" name="radioheader" value="6">
+                    <label for="radioheader_profile">Профиль</label>
+                </div>
     </header>
 </template>
 
@@ -21,7 +43,8 @@ header{
     top: 0%;
 }
 
-.buttonheader{
+.form_radio_btn_header {
+	display: inline-block;
     margin-left: 3%;
     margin-right: 3%;
     margin-top: 2%;
@@ -33,14 +56,26 @@ header{
     cursor: pointer;
     border-bottom: 2px solid white;
 }
-
-.buttonheader:hover{
-    color: $greenColor
+.form_radio_btn_header input[type=radio] {
+	display: none;
 }
-
-.buttonheader:focus{
-    color: $greenColor;
+.form_radio_btn_header label {
+	display: inline-block;
+	cursor: pointer;
+	border: none;
+	user-select: none;
+    color: $mainFontColor;
+}
+ 
+/* Checked */
+.form_radio_btn_header input[type=radio]:checked + label {
+	color: $greenColor;
     border-bottom: 2px solid $greenColor;
+}
+ 
+/* Hover */
+.form_radio_btn_header label:hover {
+    color: $greenColor
 }
 
 </style>
