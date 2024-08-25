d0571292332a44b2ca1238894c225829025b23e0
B-e-d-B-o-y <IgorPerminov2004@mail.ru>
2024-08-26 00:29:24 +0900

    Сделал страницу регистрации

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,39 @@
 <template>
     <div class="login">
     <img class="fingerprint" src="/src/assets/image/fingerprint.png"/>
-    <h1 class="titlelogin">Войти в аккаунт</h1>
-    <input id="login" class="input" placeholder="Логин">
-    <input id="password" type="password" class="input" placeholder="Пароль">
-    <button id="buttonpassword" class="button">Войти</button>
-    <div class="rectangle"><span>ИЛИ</span></div>
-    <button @click="registrarion" id="buttonregistration" class="button">Зарегистрироваться</button>
-    <slot></slot>
+
+    <div v-if="activePage === 1">
+        <h1 class="titlelogin">Войти в аккаунт</h1>
+        <input id="login" class="inputlogin" placeholder="Логин">
+        <input id="password" type="password" class="inputlogin" placeholder="Пароль">
+        <button id="buttonpassword" class="buttonlogin">Войти</button>
+        <div class="rectangle"><span>ИЛИ</span></div>
+        <button @click.prevent = "activePage = 2" id="buttonregistration" class="buttonlogin">Зарегистрироваться</button>
+    </div>
+
+    <div v-else-if="activePage === 3">
+        <h1 class="titlelogin">Код подтверждения</h1>
+        <div class="text">Код подтверждения отправлен вам на почту, если код не пришел, проверьте правильность написания почты.</div>
+        <input id="confirmation" type="number" class="inputlogin" max="9" placeholder="Введите код подтверждения">
+        <button id="buttonconfirmation" class="buttonlogin">Подтвердить</button>
+    </div>
+
+    <div v-else="activePage === 2">
+        <h1 class="titlelogin">Создать аккаунт</h1>
+        <input id="email" class="inputlogin" placeholder="Email">
+        <input id="newlogin" class="inputlogin" placeholder="Логин">
+        <input id="newpassword" type="password" class="inputlogin" placeholder="Пароль">
+        <input id="reapetpassword" type="password" class="inputlogin" placeholder="Повторить пароль">
+        <button id="registration" class="buttonlogin" @click.prevent = "activePage = 3">Зарегистрироваться</button>
+    </div>
+
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const activePage = ref(1)
 </script>
 
 <style lang="scss">
@@ -40,7 +62,7 @@ body{
     margin-bottom: 3%;
 }
 
-.input{
+.inputlogin{
     display: flex;
     border-radius: 100px;
     border: 3px solid $grayColor;
@@ -54,7 +76,7 @@ body{
     align-items: center;
 }
 
-.button{
+.buttonlogin{
     display: flex;
     border: none;
     border-radius: 100px;
@@ -100,6 +122,24 @@ body{
 .fingerprint{
     width: 10%;
     margin-left: 45%;
+}
+
+.buttonlogin:hover{
+    background-color: $greenColor;
+}
+
+.inputlogin:hover{
+    border: 3px solid $greenColor;
+}
+
+.inputlogin:focus{
+    border: 3px solid $greenColor;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
 }
 
 .text{
